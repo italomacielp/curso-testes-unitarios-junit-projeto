51483dbbaf31e4cf59512bd44b7d3e3e4b961a09
italomacielp <italo.macieldev@gmail.com>
2025-09-01 21:06:59 -0300

    🚧: Utilização de testes condicionais com Assumptions

--- src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -1,6 +1,8 @@
 package com.algaworks.junit.utilidade;
 
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 import java.time.Duration;
 
@@ -9,7 +11,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class SimuladorEsperaTest {
 
     @Test
+    @EnabledIfEnvironmentVariable(named = "ENV", matches = "DEV")
     void deveEsperarENaoDarTimeout() {
+        /*Assumptions.assumeTrue("PROD".equals(System.getenv("ENV")),
+                () -> "Abortando teste: Não deve ser executado em PROD.");*/
         assertTimeout(Duration.ofSeconds(1),
                 () -> SimuladorEspera.esperar(Duration.ofSeconds(10)));
         assertTimeoutPreemptively(Duration.ofSeconds(1),
